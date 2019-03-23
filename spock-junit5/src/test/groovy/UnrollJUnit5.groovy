09623fb10f318c50fc5287a4a97375026068fe77
lsikora <sikora.lukasz.sl@gmail.com>
2019-03-23 16:13:10 +0100

    Add test cases for running Spock on JUnit 4 and JUnit 5 Vintage

---
+++ spock-junit5/src/test/groovy/UnrollJUnit5.groovy
@@ -0,0 +1,34 @@
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class UnrollJUnit5 extends Specification {
+    @Unroll
+    def "#left is not same as #right"(Food left, Food right) {
+        expect:
+        !left.equals(right)
+
+        where:
+        left                | right
+        new Food("APPLE")   | new Food("ORANGE")
+        new Food("ORANGE")  | null
+        new Food("ORANGE")  | ""
+        new Food("GHERKIN") | new Food(null)
+        new Food("ORANGE")  | new Object()
+    }
+
+    class Food {
+        String name
+
+        Food(String name) {
+            this.name = name
+        }
+
+
+        @Override
+        String toString() {
+            return "Food{" +
+                    "name='" + name + '\'' +
+                    '}'
+        }
+    }
+}
